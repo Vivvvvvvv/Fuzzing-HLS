c4c7bd13620068092b32e23e934e09dd2b7ea1d6
Yann Herklotz <git@yannherklotz.com>
2021-01-06 17:58:03 +0000

    Add theme

--- docs/publish.el
+++ docs/publish.el
@@ -40,7 +40,9 @@
       org-html-html5-fancy t
       org-html-validation-link nil
       org-html-doctype "html5"
-      org-html-coding-system 'utf-8-unix)
+      org-html-coding-system 'utf-8-unix
+      org-html-htmlize-output-type 'css
+      org-html-htmlize-font-prefix "org-")
 
 (setq org-publish-project-alist
       (list
